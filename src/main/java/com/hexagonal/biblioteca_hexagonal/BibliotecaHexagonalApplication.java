1a0fea4f504a90eb045d1eccaedbd4a0d1b22726
Eduardo <eduardodasilvadandolini@gmail.com>
2024-09-13 19:57:49 -0300

    Initial commit

---
+++ src/main/java/com/hexagonal/biblioteca_hexagonal/BibliotecaHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.hexagonal.biblioteca_hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaHexagonalApplication.class, args);
+	}
+
+}
